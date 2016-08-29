45a9a96f2004669a3b7c89b10e38b7d310e70b75
linzewu <82367825@qq.com>
2016-08-30 00:18:41 +0800

    添加图片缓存类

---
+++ WaterFallLib/src/main/java/com/zero/waterfalllib/WaterFallManager.java
@@ -0,0 +1,35 @@
+package com.zero.waterfalllib;
+
+import android.content.Context;
+
+import com.zero.waterfalllib.cache.ImageLoader;
+
+/**
+ * @author linzewu
+ * @date 16/8/29
+ */
+public class WaterFallManager {
+    
+    private static WaterFallManager sWaterFallManager;
+    public synchronized static WaterFallManager getInstance() {
+        if (sWaterFallManager == null) {
+            sWaterFallManager = new WaterFallManager();
+        }
+        return sWaterFallManager;
+    }
+    private WaterFallManager() {
+        //private
+    }
+    
+    
+    public void init(Context context) {
+        ImageLoader.ImageLoaderBuilder imageLoaderBuilder = new ImageLoader.ImageLoaderBuilder()
+                .setDISK_CACHE_ENABLED()
+                .setDISK_CACHE_NAME()
+                .setDISK_CACHE_SIZE()
+                .setMEM_CACHE_SIZE()
+                .setMEN_CACHE_ENABLED()
+                .setLOAD_WAY_NOW();
+        ImageLoader.getInstance().init(context.getApplicationContext(), imageLoaderBuilder);
+    }
+}
