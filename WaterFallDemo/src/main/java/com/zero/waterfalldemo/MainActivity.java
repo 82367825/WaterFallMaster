37a59998398db3fe11806b3405d5250cede1ad9e
linzewu <82367825@qq.com>
2016-09-14 19:39:21 +0800

    添加瀑布流代码

--- WaterFallDemo/src/main/java/com/zero/waterfalldemo/MainActivity.java
+++ WaterFallDemo/src/main/java/com/zero/waterfalldemo/MainActivity.java
@@ -39,8 +39,11 @@ public class MainActivity extends Activity {
         "http://img.taopic.com/uploads/allimg/130710/267873-130G011000550.jpg",
             "http://img5.imgtn.bdimg.com/it/u=1104713303,1290055170&fm=21&gp=0.jpg",
             "http://img3.imgtn.bdimg.com/it/u=761080875,1194258054&fm=21&gp=0.jpg",
-            "http://img5.imgtn.bdimg.com/it/u=2614807623,3042537006&fm=21&gp=0.jpg",
             "http://img0.imgtn.bdimg.com/it/u=1906189186,1400341202&fm=21&gp=0.jpg",
-            "http://img0.imgtn.bdimg.com/it/u=1953294160,3802419744&fm=11&gp=0.jpg"
+            "http://imgsrc.baidu.com/forum/w=580/sign=34f7ea61d443ad4ba62e46c8b2005a89" +
+                    "/78e5fc039245d68839fba560a1c27d1ed31b2415.jpg",
+            "http://img0.imgtn.bdimg.com/it/u=1953294160,3802419744&fm=11&gp=0.jpg",
+            "http://imga.mumayi.com/android/wallpaper/2012/01/21/sl_600_2012012105503827801816.jpg",
+            "http://img5.imgtn.bdimg.com/it/u=2614807623,3042537006&fm=21&gp=0.jpg"
     };
 }
